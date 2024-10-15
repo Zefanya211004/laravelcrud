dced8b205c1014dd871cb3f941c293ef678e847b
Zefanya211004 <zefanyatfs21@gmail.com>
2024-10-16 00:47:22 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    $posts = [];
+    if (auth()->check()) {
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    } 
+    //$posts = Post::where('user_id', auth()->id())->get();
+    return view('home', ['posts' => $posts]);
+});
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/login', [UserController::class, 'login']);
+
+//Blog post related routes
+Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
+// Route::view('/upload', 'upload');
+// Route::post('/upload', [PostController::class, 'upload'])->name('');
